0f095b3d6da1af755996f74beb90fc70aa858712
Lyudmila-web <80260973+Lyudmila-web@users.noreply.github.com>
2021-08-12 08:22:00 -0300

    Add files via upload

---
+++ nafta.js
@@ -0,0 +1,16 @@
+const Calcular = () => {
+const precioCombustible = document.getElementById("combustible").value;
+console.log(precioCombustible);
+
+}
+
+
+
+//tomar el rendimiento
+//Tomar los km a recorrer
+
+
+
+function calcularES5(){
+
+}
